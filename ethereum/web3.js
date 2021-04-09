c9886745df79475ac752b452ca0b91f3af1bc314
nlsjr <nelson.sjr.nsj@gmail.com>
2021-04-09 09:06:18 -0300

    fix provider

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,14 +1,11 @@
 import Web3 from 'web3';
+const config = require("../environmentConfig.json");
+
 
 let web3;
 
-if(typeof window !== 'undefined' && window.web3 !== 'undefined') {
-    //we are ub the browser and metamask is running.
-    web3 = new Web3(window.web3.currentProvider);
-} else {
-    //we are on the server or the user is not running metamask
-    const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/dd6dabd904c44c478dc6bfe7b22ca036');
-    web3 = new Web3(provider);
-}
+//we are on the server or the user is not running metamask
+const provider = new Web3.providers.HttpProvider(config.dev.ethereum.testnet.infuraApi);
+web3 = new Web3(provider);
 
 export default web3;
